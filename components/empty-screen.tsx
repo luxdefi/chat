8e6f7ea4aea3f4bea2bba9d12e1e4ca98a2b88f2
Zach Kelling <z@zeekay.io>
2023-12-09 03:38:45 +0100

    Update starting prompts

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Learn about Lux Network',
+    message: `What is a Lux Network?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Learn about Lux Validator',
+    message: 'Give me instructions on how to run a Lux Validator.'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Get a Lux Credit Card',
+    message: `How do I applt for Lux Credit?`
   }
 ]
 
@@ -24,16 +24,8 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Hi, I'm Lux AI.
         </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
         </p>
